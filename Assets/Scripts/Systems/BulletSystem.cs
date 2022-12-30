4d5ebbc3f661de2a0157161b8724f9908ed1fafe
I-Bearman <i-medvedev92@yandex.ru>
2022-12-30 12:53:04 +0700

    Changes 29/12

--- Assets/Scripts/Systems/BulletSystem.cs
+++ Assets/Scripts/Systems/BulletSystem.cs
@@ -3,30 +3,20 @@ using Unity.Entities;
 
 public class BulletSystem : ComponentSystem
 {
-    private EntityManager entityManager;
     private EntityQuery _bulletQuery;
-    private float _currLifeTime;
     protected override void OnCreate()
     {
         _bulletQuery = GetEntityQuery(ComponentType.ReadOnly<BulletComponent>());
-        //entityManager.CreateEntityQuery(ComponentType.ReadOnly<BulletComponent>());
     }
 
-    protected override void OnStartRunning()
-    {
-        _currLifeTime = 0;
-    }
     protected override void OnUpdate()
     {
         Entities.With(_bulletQuery).ForEach(
-        (Entity entity, Rigidbody rigidbody, BulletComponent bulletComponent) =>
+        (Entity entity, Transform transform, Rigidbody rigidbody, BulletComponent bulletComponent) =>
         {
-            rigidbody.velocity = new Vector3(0, 0, bulletComponent.speed);
-            _currLifeTime += Time.DeltaTime;
-            if (_currLifeTime > bulletComponent.lifeTime)
+            if (rigidbody.velocity == Vector3.zero)
             {
-                //entityManager.DestroyEntity(entity);
-                
+                rigidbody.velocity = transform.forward * bulletComponent.speed;
             }
         });
     }
--- Assets/Scripts/Systems/CharacterMoveSystem.cs
+++ Assets/Scripts/Systems/CharacterMoveSystem.cs
@@ -4,10 +4,19 @@ using Unity.Entities;
 public class CharacterMoveSystem : ComponentSystem
 {
     private EntityQuery _moveQuery;
+    private Rigidbody _rigidbody;
 
     protected override void OnCreate()
     {
         _moveQuery = GetEntityQuery(ComponentType.ReadOnly<InputData>(), ComponentType.ReadOnly<MoveData>(), ComponentType.ReadOnly<Transform>());
+    }
+    protected override void OnStartRunning()
+    {
+        Entities.With(_moveQuery).ForEach(
+            (Entity entity, Rigidbody rigidbody, ref InputData inputData, ref MoveData move) =>
+            {
+                _rigidbody = rigidbody;
+            });
     }
     protected override void OnUpdate()
     {
@@ -22,9 +31,11 @@ public class CharacterMoveSystem : ComponentSystem
                     transform.LookAt(lookDirection * 100);
                 }
 
-                var pos = transform.position;
-                pos += new Vector3(inputData.Move.x* move.Speed, 0, inputData.Move.y* move.Speed);
-                transform.position = pos;
+                /*var pos = transform.position;
+                pos += new Vector3(inputData.Move.x * move.Speed, 0, inputData.Move.y * move.Speed);
+                transform.position = pos;*/
+
+                _rigidbody.AddForce(new Vector3(inputData.Move.x * move.Speed, 0, inputData.Move.y * move.Speed), ForceMode.Force);
             });
     }
 }
