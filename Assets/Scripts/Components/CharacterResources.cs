77fea8aaeb4f447281ac1087aeac0636f5bf3a6c
I-Bearman <i-medvedev92@yandex.ru>
2025-02-18 17:12:55 +0700

    PreFinal Changes HomeWork 4

--- Assets/Scripts/Components/CharacterResources.cs
+++ Assets/Scripts/Components/CharacterResources.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Networking;
 
 public class CharacterResources : MonoBehaviour
 {
     public PlayerResources playerResources;
+    [SerializeField] private Text goldText;
+    [SerializeField] private string jsonResourcesUrl;
+    private string jsonText;
 
     private void Awake()
     {
-        var fileList = GoogleDriveTools.FileList();
-        Debug.Log(fileList);
+        StartCoroutine(LoadTextFromServer(jsonResourcesUrl));
+        playerResources = JsonUtility.FromJson<PlayerResources>(jsonText);
     }
+
+    IEnumerator LoadTextFromServer(string url)
+    {
+        var request = UnityWebRequest.Get(url);
+
+        yield return request.SendWebRequest();
+
+        if (!request.isHttpError && !request.isNetworkError)
+        {
+            jsonText = request.downloadHandler.text;
+        }
+        else
+        {
+            Debug.LogErrorFormat("error request [{0}, {1}]", url, request.error);
+        }
+
+        request.Dispose();
+    }
+
+    private void Start()
+    {
+        
+    }
+
     private void WriteStatistics()
     {
-        var jsonString = JsonUtility.ToJson(playerResources.gold);
-        Debug.Log(jsonString);
-        PlayerPrefs.SetString("PlayerResources", jsonString);
-        PlayerPrefs.Save();
+
     }
 }
