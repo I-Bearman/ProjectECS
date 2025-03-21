cb27d104ac856a5ffe784d26a34072da7f3927e0
I-Bearman <i-medvedev92@yandex.ru>
2025-03-21 15:16:34 +0700

    A little change 21/03

--- Assets/Scripts/Installers/SettingsInstaller.cs
+++ Assets/Scripts/Installers/SettingsInstaller.cs
@@ -4,8 +4,15 @@ using Zenject;
 [CreateAssetMenu(fileName = "SettingsInstaller", menuName = "Installers/SettingsInstaller")]
 public class SettingsInstaller : ScriptableObjectInstaller<SettingsInstaller>
 {
+    GameConfigurationsDummy configurationsDummy;
     public override void InstallBindings()
     {
         Container.BindInterfacesAndSelfTo<GameConfigurations>().AsSingle();
+        Container.Bind<GameConfigurationsDummy>().FromInstance(configurationsDummy);
     }
+}
+
+class GameConfigurationsDummy
+{
+    Screen.SetResolution(800, 600, false);
 }
